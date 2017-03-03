d3f086d8e09f12af6c8bc6b8a03c014c9b2da519
Maxime <maxime.antoine2@gmail.com>
2017-03-03 19:06:23 +0800

    changed client side authentication logic

--- src/Yasas.Web/wwwroot/js/app.js
+++ src/Yasas.Web/wwwroot/js/app.js
@@ -6,26 +6,25 @@
             .when('/', {
                 name: 'home',
                 templateUrl: 'templates/home.html',
-                allowAnonymous: true
             })
             .when('/login', {
                 name: 'login',
                 templateUrl: 'templates/login.html',
-                allowAnonymous: true
             })
             .when('/admin', {
                 name: 'admin',
-                templateUrl: 'templates/admin.html'
+                templateUrl: 'templates/admin.html',
+                authorize: true,
+                requiredRoles: ['admin']
             })
             .otherwise({
                 redirectTo: '/',
-                allowAnonymous: true //allow the REDIRECTION to anybody
             })
     }]);
 
     app.run(['$rootScope','$location','authenticationSvc', function ($rootScope, $location, authenticationSvc) {
         $rootScope.$on('$routeChangeStart', function (event, next, current) {
-            if (!authenticationSvc.isLoggedIn() && !next.allowAnonymous)
+            if (!authenticationSvc.isLoggedIn() && next.authorize)
                 if (next.name !== 'login')
                     $location.path('/login');
         });
