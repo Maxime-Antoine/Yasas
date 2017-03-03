48e6fc1b1bdfbaee963e54777269a0de6d6cd19d
Maxime <maxime.antoine2@gmail.com>
2017-03-03 18:15:11 +0800

    client side routing draft

--- src/Yasas.Web/wwwroot/js/app.js
+++ src/Yasas.Web/wwwroot/js/app.js
@@ -1,3 +1,33 @@
 ﻿(function () {
-    var app = angular.module("Yasas", []);
+    var app = angular.module('YASAS', ['ngRoute']);
+
+    app.config(["$routeProvider", function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                name: 'home',
+                templateUrl: 'templates/home.html',
+                allowAnonymous: true
+            })
+            .when('/login', {
+                name: 'login',
+                templateUrl: 'templates/login.html',
+                allowAnonymous: true
+            })
+            .when('/admin', {
+                name: 'admin',
+                templateUrl: 'templates/admin.html'
+            })
+            .otherwise({
+                redirectTo: '/',
+                allowAnonymous: true //allow the REDIRECTION to anybody
+            })
+    }]);
+
+    app.run(['$rootScope','$location','authenticationSvc', function ($rootScope, $location, authenticationSvc) {
+        $rootScope.$on('$routeChangeStart', function (event, next, current) {
+            if (!authenticationSvc.isLoggedIn() && !next.allowAnonymous)
+                if (next.name !== 'login')
+                    $location.path('/login');
+        });
+    }]);
 })()
---
+++ src/Yasas.Web/wwwroot/js/services/authenticationService.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var app = angular.module('YASAS');
+
+    app.service('authenticationSvc', function () {
+        var loggedIn = false;
+
+        this.login = function () {
+            loggedIn = true;
+        };
+
+        this.logout = function () {
+            loggedIn = false;
+        };
+
+        this.isLoggedIn = function () {
+            return loggedIn;
+        }
+    });
+})();
