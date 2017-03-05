445400ddca48dd06aef2ec8f443388aa2c284421
Maxime <maxime.antoine2@gmail.com>
2017-03-06 00:51:20 +0800

    Removed unused dependency

--- src/Yasas.Web/Startup.cs
+++ src/Yasas.Web/Startup.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
